f12acc712bd6bcdf63b930046d5d8aea59cfefe9
Samuel Havron <sgh7cc@virginia.edu>
2017-09-28 20:47:27 -0400

    clarification on skipping lines

--- scrape.py
+++ scrape.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 profile = webdriver.FirefoxProfile()
-profile.set_preference("general.useragent.override", "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0") # spoof mobile agent
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0")
 browser = webdriver.Firefox(profile) # geckodriver needs to be in your PATH
 browser.get('https://www.google.com')
 
 # save data for later parsing/visual inspection... #noratelimitspls
 with open('spyware_queries.csv', 'r') as file_handle:
     queries = csv.reader(file_handle, delimiter=',')
-    for i in range(0,447):
-        queries.next()
+    #for i in range(0,447): ### needed to skip some lines for debugging
+    #    queries.next()
     for row in queries:
         row[1] = ast.literal_eval(row[1])
         for term in row[1]:
