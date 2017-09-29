23dd9af268bdc7bba4e1c52b7481e0b2fce6e617
Samuel Havron <sgh7cc@virginia.edu>
2017-09-28 20:42:10 -0400

    searched, not parsed

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A simple scraper for Google Ads on search results, using selenium to simulate
 Firefox. Ensure the latest version of selenium (and Firefox's gecko driver) are
 installed.
+
+Currently, this scraper simply saves the source (and screenshot) for later
+parsing.
--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import csv, time, urlparse
+import csv, time, urlparse, os, ast
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -7,31 +7,30 @@ profile.set_preference("general.useragent.override", "Mozilla/5.0 (Android 4.4; 
 browser = webdriver.Firefox(profile) # geckodriver needs to be in your PATH
 browser.get('https://www.google.com')
 
-query = browser.find_element_by_name('q')
-query.send_keys("toothbrush") # reliably returns ads for us to try to parse...
-query.send_keys(Keys.RETURN) # hit return after you enter search text
-browser.implicitly_wait(5)
-
-##### beginning of attempt to parse links
-links = browser.find_elements_by_xpath("//h3[@class='r']/a[@href]")
-results = []
-for link in links:
-    title = link.text.encode('utf8')
-    url = link.get_attribute('href')
-    results.append((title, url[29:])) # take out 'https://www.google.com/url?q=' from string, which is 29 chars
-
-for r in results:
-    print(r)
-##### end of attempt to parse links
-exit(0) # TODO: remove this when ready to integrate actual spyware queries template code below
-
+# save data for later parsing/visual inspection... #noratelimitspls
 with open('spyware_queries.csv', 'r') as file_handle:
     queries = csv.reader(file_handle, delimiter=',')
+    for i in range(0,447):
+        queries.next()
     for row in queries:
-        query = browser.find_element_by_name('q')
-        query.send_keys(row[0]) # search by an actual spyware query
-        query.send_keys(Keys.RETURN) # hit return after you enter search text
-        browser.implicitly_wait(5)
+        row[1] = ast.literal_eval(row[1])
+        for term in row[1]:
+            query = browser.find_element_by_name('q')
+            query.send_keys(term) # search by an actual spyware query
+            query.send_keys(Keys.RETURN) # hit return after you enter search text
+            term = term.replace(" ", "_") # make filenames cleaner
+            time.sleep(2)
+            path = "./ads/"+row[0]+"/"
+            path = path.replace(" ", "_")
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            browser.save_screenshot(path+term+".png")
+            with open(path+term+".html", 'w') as hits_handle:
+                hits_handle.write(browser.page_source.encode('utf-8').strip())
+
+            print("Saved all hits for '"+path+term+"'...")
+            browser.get('https://www.google.com')
+
         # TODO: here, parse out the ad links and store them in a new file/print them out
-        browser.get('https://www.google.com') 
         # print(', '.join(row))
